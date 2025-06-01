539552f0b04ca8fd23ce9a4e99b5fa5497f7d407
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-06-01 17:58:20 +0600

    setup

---
+++ ifelse_lader.js
@@ -0,0 +1,16 @@
+var name="sujon";
+
+if (name=="sujon"){
+    console.log("oho sujon soki go");
+}
+else{
+    console.log("how aer you");
+}
+
+var result=3;
+
+if (result>=0 && result< 10)
+{
+    console.log("tumi c grade piso ");
+}
+else if()
---
+++ object.js
@@ -0,0 +1,13 @@
+var result=80;
+
+var person={
+    hand:2,
+    eye:2,
+    hair:"dont know",
+    sujon:{
+        hand:3,
+        eye:4,
+        clg:"tud tud mrist"
+    }
+}
+console.log(person);
