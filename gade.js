85bd5d27afc5dc5cffd4d632824e5152c8e31f94
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-06-03 17:50:30 +0600

    problem done

--- gade.js
+++ gade.js
@@ -1,30 +1,41 @@
- var friends = ["Brahmi", "karim", "abdul", "sadsd", "heroAlom"];
+//  var friends = ["Brahmi", "karim", "abdul", "sadsd", "heroAlom"];
 
- let big_name=friends[0];
- for (let i = 0; i < friends.length; i++) {
-    let element=friends[i]
-    if(big_name.length<element.length){
-         big_name=element;
-    }
+//  let big_name=friends[0];
+//  for (let i = 0; i < friends.length; i++) {
+//     let element=friends[i]
+//     if(big_name.length<element.length){
+//          big_name=element;
+//     }
     
     
-}
-console.log(big_name);
-let levaing_cost=[];
-const monetlySaving=(Array,num)=>{
+// }
+// console.log(big_name);
+ 
+const monetlySaving=(pamant,levaing_cost)=>{
 
-     for (let i = 0; i < Array.length; i++) {
-        const element = Array[i];
+      let total_pamant=0;
+      let total_tax=0;
+     for (let i = 0; i < pamant.length; i++) {
+        let element = pamant[i];
+         total_pamant+=element;
         if(element>=3000){
-            levaing_cost=element%20;
-            return levaing_cost;
-
+             const tax= element *0.20;
+             total_tax+=tax;    
         }
         
      }
-     if(levaing_cost==0){
-        console.log("taka ni ");
-     }
+      let savinges=total_pamant-total_tax-levaing_cost;
+      if(savinges<=0)
+      {
+         return 0;
+      }
+      else{
+         return savinges;
+      }
 }
-const res=monetlySaving([1000,2000,2500],204);
+const res=monetlySaving([1000,2000,2500],5000);
 console.log(res);
+
+let numbers=[1,2,3,3,4,4,5,6,7,8,9,10];
+let maxnum=Math.max(...numbers);
+console.log(maxnum);
